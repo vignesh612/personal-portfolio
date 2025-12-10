bea472ba8313f4ce3d27f5b0a6b8f830e4f2f285
vignesh612 <kvb612@gmail.com>
2025-12-10 11:50:09 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { Analytics } from "@vercel/analytics/react"
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
